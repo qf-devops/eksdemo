d1f039b6472071fd1e42badaeb64ce94eaa5cae0
ravi <ravi@vytec.com>
2023-09-29 10:44:54 +0530

    pvc

--- ebs.sh
+++ ebs.sh
@@ -1,1 +1,4 @@
 aws ec2 create-volume --region ap-northeast-1 --availability-zone ap-northeast-1a --size10 --volume-type gp2
+
+
+eksctl create addon --name aws-ebs-csi-driver --cluster fargate-cluster --service-account-role-arn arn:aws:iam::675467602881:role/AmazonEKS_EBS_CSI_DriverRole --force
